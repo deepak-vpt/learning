5c9962e6fcfd3633262ba45d969e58b58bbf22a2
deepak-vpt <deepakraj11520@gmail.com>
2024-08-17 18:53:28 +0530

    project 1  basic tailwind

---
+++ html/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      backgroundImage: theme => ({
+        'hero-pattern': "url('https://ik.imagekit.io/deepakvpt/jarvis.png?updatedAt=1723178801885')",
+        'footer-texture': "url('https://ik.imagekit.io/deepakvpt/jarvis.png?updatedAt=1723178801885')",
+      }),
+    },
+  },
+  variants: {},
+  plugins: [],
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: theme => ({
+          'hero-pattern': "url('https://ik.imagekit.io/deepakvpt/jarvis.png?updatedAt=1723178801885')",
+          'footer-texture': "url('https://ik.imagekit.io/deepakvpt/jarvis.png?updatedAt=1723178801885')",
+        }),
+      },
     },
-    plugins: [],
-  }/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
+    variants: {},
     plugins: [],
   }
+  
